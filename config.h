7e0c84338323573ee64250b51318d3959ce1368f
Nick Friday <nfriday@ya.ru>
2024-04-29 05:24:24 +0300

    ze

--- config.h
+++ config.h
@@ -1,13 +1,14 @@
 // the blocks themselves
 static const Block blocks[] = {
-  // Command,                                   Update Interval,  Update Signal,  Sub To Signal
+  // Command,                                   Update Interval,  Update Signal,
+  // Sub To Signal
   // clang-format off
   { "getplayerctl '^c#6272A4^no active player^c#f8f8f2^'",  60,   21,  "playerctl -F metadata 2> /dev/null | subtosignal \"pkill -RTMIN+21 rootblocks\"" },
   { "db-volume",                                            60,   10,  "pactl subscribe | grep --line-buffered \"sink #\" | subtosignal \"pkill -RTMIN+10 rootblocks\"" },
-  { "echo \"^c#FF92D0^$(xkb-switch)^c#f8f8f2^\"",           60,   12,  "xkb-switch -W | subtosignal \"pkill -RTMIN+12 rootblocks\"" },
+  // { "echo \"^c#FF92D0^$(xkb-switch)^c#f8f8f2^\"",           60,   12,  "xkb-switch -W | subtosignal \"pkill -RTMIN+12 rootblocks\"" },
   // { "db-bat",                                               5,    20,  "upower -m | subtosignal \"pkill -RTMIN+20 rootblocks\"" },
   /* { "twnextdue",                                            10,   0,   "" }, */
-  { "muttupd",                                              10,   0,   "" },
+  // { "muttupd",                                              10,   0,   "" },
   { "rssupd",                                               10,   0,   "" },
   { "date '+^c#ff92d0^%b %-d, %a^c#f8f8f2^'",               60,   0,   "" },
   { "date '+^c#8be9fd^%I:%M %p^c#f8f8f2^'",                 1,    0,   "" },
